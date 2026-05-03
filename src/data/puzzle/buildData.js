6713f1d02fc2c8c3d3cb8dafbe60fafd40d4820c
jhonyx00 <jbrr.jxr@gmail.com>
2026-05-02 22:38:05 -0600

    refactor: resize 2x2x2, 3x3x3, 4x4x4, 3x3x4 and 3x3x5 puzzles to optimize viewport fit

--- src/data/puzzle/buildData.js
+++ src/data/puzzle/buildData.js
@@ -3,7 +3,7 @@
  */
 const BUILD_DATA = {
     "2x2x2": {
-        cubeletSize: 80,
+        cubeletSize: 82,
         maxPositionFactor: 0.5,
         order: { w: 2, h: 2, d: 2, },
         indexToAxis: { 0: -0.5, 1: 0.5 },
@@ -23,26 +23,26 @@ const BUILD_DATA = {
         indexToAxis: { 0: -1, 1: 0, 2: 1 },
     },
     "3x3x3": {
-        cubeletSize: 60,
+        cubeletSize: 64,
         maxPositionFactor: 1,
         order: { w: 3, h: 3, d: 3, },
         indexToAxis: { 0: -1, 1: 0, 2: 1 },
     },
     "4x4x4": {
-        cubeletSize: 48,
+        cubeletSize: 50,
         maxPositionFactor: 1.5,
         order: { w: 4, h: 4, d: 4, },
         indexToAxis: { 0: -1.5, 1: -0.5, 2: 0.5, 3: 1.5 },
     },
     "3x3x4": {
-        cubeletSize: 52,
+        cubeletSize: 54,
         maxPositionFactor: 1,
         order: { w: 3, h: 3, d: 4, },
         offset: { x: 0, y: -1, z: 0 },
         indexToAxis: { 0: -1, 1: 0, 2: 1, 3: 2, 4: 3 },
     },
     "3x3x5": {
-        cubeletSize: 52,
+        cubeletSize: 54,
         maxPositionFactor: 1,
         order: { w: 3, h: 3, d: 5, },
         offset: { x: 0, y: -1, z: 0 },
